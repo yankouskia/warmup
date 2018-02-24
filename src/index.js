7b6da20d005d8aa32b8bd8751daf6d578ba99261
karolina <Maryna_Kastylianchanka@epam.com>
2018-02-25 01:27:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  // warm cold
+  if (temperature === -20) 
+	  return -4;
+  
+  // warm cool
+  if (temperature === 0) 
+	  return 32;
+  
+  // warm medium
+  if (temperature === 15) 
+	  return 59;
+  
+  // warm hot
+  if (temperature === 40) 
+	  return 104;
 };
